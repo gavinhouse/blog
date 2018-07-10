13991a2418ba01037361df993ec8a78e33cb05ed
Gavin <gavin.house@ttu.edu>
2018-07-10 12:09:47 +0100

    Added current section highlight in navbar

--- application/libraries/Navigation.php
+++ application/libraries/Navigation.php
@@ -6,14 +6,27 @@
  */
 class Navigation{
 
-    function navbarLink($pageName, $directory){
+    public function navbarLink($pageName, $directory){
+        if($this->isActive($directory)){
+            $active = ' active';
+        }
+        else{
+            $active = FALSE;
+        }
         $directory = site_url($directory);
+
         return <<<HTML
-<li class="nav-item">
+<li class="nav-item{$active}">
     <a class="nav-link" href="{$directory}">{$pageName}</a>
 </li>
 HTML;
+    }
 
+    private function isActive($directory){
+        $currentURI = explode('/',$_SERVER['REQUEST_URI']);
+        $directory = explode('/', $directory);
+
+        return ($currentURI[2] === $directory[0]) || ($currentURI[2] == 'users' && $directory[0] === 'authors');
     }
 
 }
--- application/views/shared/navbar.php
+++ application/views/shared/navbar.php
@@ -4,20 +4,21 @@ $this->load->library('Navigation');
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light" style="<?= $style ?>">
-<a class="navbar-brand" href="<?php echo site_url('home/index');?>">Blogger's Digest</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
+    <a class="navbar-brand" href="<?php echo site_url('home/index');?>">Blogger's Digest</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
 
-        <?= $this->CI->navigation->navbarLink('Posts Table', 'posts/index'); ?>
-        <?= $this->CI->navigation->navbarLink('Authors Table', 'authors/index'); ?>
+            <?= $this->CI->navigation->navbarLink('Posts Table', 'posts/index'); ?>
+            <?= $this->CI->navigation->navbarLink('Authors Table', 'authors/index'); ?>
 
-    </ul>
-</div>
+        </ul>
+    </div>
 </nav>
+
 <div id="content">
     <br />
 </div>
