9924e776efda9d4f999b7351bf0aa47e7d44bbd6
Gavin <gavin.house@ttu.edu>
2018-07-11 12:01:55 +0100

    Aligned text

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,9 +1,9 @@
 <div id="content" >
-    <div id="main-menu">
+    <div id="main-menu" align="center">
         <h1>Welcome to The Blogger's Digest!</h1>
         <br />
         <br />
-        <h4>Visit either the post section or the author section:</h4>
+        <h4>Log in or create an account to view blogs</h4>
         <br />
         <br />
 
