71b3ac9ca342e1c8686f2b0a229dd969bac3e032
Gavin <gavin.house@ttu.edu>
2018-07-11 11:58:43 +0100

    Fixed spacing

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -6,16 +6,7 @@
         <h4>Visit either the post section or the author section:</h4>
         <br />
         <br />
-        <div class="row">
-            <div class="column" style="float: left; position: relative; left: 20px">
-                <h7>Post Table:</h7>
-                 <a href="<?php echo site_url('/posts/index');?>"><img src="<?php echo base_url('images/blog.jpg')?>" alt="blog" style="width:100%"></a>
-            </div>
-            <div class="column" style="float: left; position: relative; left: 30px">
-                <h7>Author Table:</h7>
-                <a href="<?php echo site_url('/users/login');?>"><img src="<?php echo base_url('images/author.jpg')?>" alt="author" style="width:70.5%"></a>
-            </div>
-        </div>
+
     </div>
 </div>
 
