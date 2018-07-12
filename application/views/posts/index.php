681b4e7e80925093ab8410c953e35de56ff8f93f
Gavin <gavin.house@ttu.edu>
2018-07-12 09:51:24 +0100

    Styling for posts page(s)

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,35 +1,85 @@
+<style>
+    .post-listing{
+        padding-left: 25%;
+        padding-right: 25%;
+    }
+</style>
 <div id="content">
-    <div class="post listing">
+    <div class="post-listing" align="center" >
 
         <div class="actions">
             <a class="action" href="<?php echo site_url('posts/create'); ?>">Add New Post</a>
         </div>
 
-        <table class="table table-striped">
-            <thead class="thead-dark">
-            <tr>
-                <th>ID</th>
-                <th>Author Name</th>
-                <th>Title</th>
-                <th>Content</th>
-                <th> </th>
-                <th> </th>
-            </tr>
-            </thead>
 
-            <?php foreach($posts as $subject){?>
-                <tr>
-                    <td><?php echo $subject['id'];?></td>
-                    <td><?= $subject['authorName'];?></td>
-                    <td><?php echo $subject['title'];?></td>
-                    <td style="white-space:pre-wrap; word-wrap:break-word"><?php echo $subject['content'];?></td>
-                    <?php if($_SESSION['username'] === $subject['authorName'] || $_SESSION['username'] === 'admin'){?>
-                        <td><a class="action" href="<?php echo site_url('posts/edit/'.$subject['id']) ?>">Edit</a></td>
-                        <td><a class="action" href="<?php echo site_url('posts/delete/'.$subject['id']) ?>">Delete</a></td>
-                    <?php } ?>
-                </tr>
-            <?php }  ?>
-        </table>
+
+        <div class="w3-card-4 w3-margin w3-white">
+            <img src="../../../images/author.jpg" alt="Nature" style="width:100%" height="320px">
+            <div class="w3-container">
+                <h3><b>TITLE HEADING</b></h3>
+                <h5>Title description, <span class="w3-opacity">April 7, 2014</span></h5>
+            </div>
+
+            <div class="w3-container">
+                <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed
+                    tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed
+                    tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla</p>
+                <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                        <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                        <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+            <!-- Blog entry -->
+            <div class="w3-card-4 w3-margin w3-white">
+                <img src="/w3images/woods.jpg" alt="Nature" style="width:100%">
+                <div class="w3-container">
+                    <h3><b>TITLE HEADING</b></h3>
+                    <h5>Title description, <span class="w3-opacity">April 7, 2014</span></h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed
+                        tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+                    <div class="w3-row">
+                        <div class="w3-col m8 s12">
+                            <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                        </div>
+                        <div class="w3-col m4 w3-hide-small">
+                            <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <!-- Blog entry -->
+            <div class="w3-card-4 w3-margin w3-white">
+                <img src="/w3images/bridge.jpg" alt="Norway" style="width:100%">
+                <div class="w3-container">
+                    <h3><b>BLOG ENTRY</b></h3>
+                    <h5>Title description, <span class="w3-opacity">April 2, 2014</span></h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed
+                        tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+                    <div class="w3-row">
+                        <div class="w3-col m8 s12">
+                            <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                        </div>
+                        <div class="w3-col m4 w3-hide-small">
+                            <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-badge">2</span></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php
         if($action === 'add'){
             echo '<p>Post added successfully!</p>';
